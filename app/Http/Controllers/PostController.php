727378afb8c0b1d28f1570eafec84cf9005969cd
111100001 <fqf1fqf@gmail.com>
2025-07-25 17:37:42 +0300

    feat: Enhance Post creation functionality with validation, image upload, and category selection

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -24,7 +26,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::get();
+        return view('post.create',['categories' => $categories]);
     }
 
     /**
@@ -32,7 +35,28 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required',
+            'category_id' => 'required|exists:categories,id',
+            'published_at' => ['nullable', 'datetime'],
+        ]);
+        /**@var \Illuminate\Http\UploadedFile $image */  
+
+        $image = $data['image'];
+        unset($data['image']);
+        $data['user_id'] = Auth::id();
+        $data['slug'] = Str::slug($data['title']) . '-' . Str::random(6);
+
+        $imagePath = $image->store('posts', 'public');
+        $data['image'] = $imagePath;
+
+
+        
+        Post::create($data);
+
+        return redirect()->route('dashboard');
     }
 
     /**
